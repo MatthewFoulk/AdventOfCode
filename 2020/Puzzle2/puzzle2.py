5a6fb6e3b7f8ef647d5913caafaecdc45e2eccf2
Matthew Foulk <mewuzhere12@gmail.com>
2020-12-02 08:14:34 -0500

    Added puzzle2

---
+++ 2020/Puzzle2/puzzle2.py
@@ -0,0 +1,62 @@
+
+import pandas as pd
+import re
+
+FILE_NAME = 'input.txt'
+
+
+def firstPart():
+    validCount = 0 # Number of valid passwords
+
+    regExp = re.compile(r'(?P<min>\d+)-(?P<max>\d+)\s(?P<letter>\w):\s(?P<password>\w+)') # Regexp for parsing lines
+
+    with open(FILE_NAME, 'r') as f:
+        for line in f:
+            match = regExp.match(line)
+            if match:
+                minNum = int(match.group('min'))
+                maxNum = int(match.group('max'))
+                letter = match.group('letter')
+                password = match.group('password')
+
+                # Check if password meets criteria
+                # The letter must occur a number of times
+                # in range [minNum, maxNum]
+                letterCount = password.count(letter)
+                if letterCount >= minNum and letterCount <= maxNum:
+                    validCount += 1
+    return validCount
+
+def secondPart():
+    validCount = 0 # Number of valid passwords
+
+    regExp = re.compile(r'(?P<firstPos>\d+)-(?P<secondPos>\d+)\s(?P<letter>\w):\s(?P<password>\w+)') # Regexp for parsing lines
+
+    with open(FILE_NAME, 'r') as f:
+        for line in f:
+            match = regExp.match(line)
+            if match:
+                firstPos = int(match.group('firstPos')) - 1 # -1 for zero indexing
+                secondPos = int(match.group('secondPos')) - 1 # -1 for zero indexing
+                letter = match.group('letter')
+                password = match.group('password')
+
+                
+
+                # Check if password meets criteria
+                # Exactly one position must match the letter
+                if ((password[firstPos] == letter) != 
+                    (password[secondPos] == letter)):
+                    validCount += 1
+    return validCount
+
+def main():
+
+    firstAnswer = firstPart()
+    print(f"First Answer: {firstAnswer}")
+
+    secondAnswer = secondPart()
+    print(f"Second Answer: {secondAnswer}")
+
+if __name__ == "__main__":
+    main()
