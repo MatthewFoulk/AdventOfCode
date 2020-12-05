59431ae7aa5b1836a769d471934b96fe1f75b121
Matthew Foulk <mewuzhere12@gmail.com>
2020-12-05 12:17:56 -0500

    Initial puzzle6 commit

---
+++ 2020/Puzzle6/puzzle6.py
@@ -0,0 +1,22 @@
+
+def readInput(inputFile):
+
+    with open(inputFile, 'r') as f:
+        pass
+
+def firstPart(data):
+    pass
+
+def secondPart(data):
+    pass
+
+def main():
+
+    firstAnswer = firstPart(data)
+    print(f"First Answer: {firstAnswer}")
+
+    secondAnswer = secondPart(data)
+    print(f"Second Answer: {secondAnswer}")
+
+if __name__ == "__main__":
+    main()
